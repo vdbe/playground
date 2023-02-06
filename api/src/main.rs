1845b669fa8de742c39c6e6e0dc7a03b6fc831a8
vdbewout <vdbewout@gmail.com>
2023-02-06 11:00:45 +0100

    Feat: Hello, World!

---
+++ api/src/main.rs
@@ -0,0 +1,82 @@
+use std::net::{IpAddr, SocketAddr};
+
+use axum::{response::Html, routing::get, Router};
+use clap::Parser;
+use tokio::signal;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::EnvFilter;
+
+#[derive(Debug, Parser)]
+pub struct Config {
+    #[clap(long, default_value = "127.0.0.1", env)]
+    pub host: IpAddr,
+    #[clap(long, default_value = "3000", env)]
+    pub port: u16,
+}
+
+impl Config {
+    pub fn host(&self) -> IpAddr {
+        self.host
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    #[cfg(debug_assertions)]
+    dotenv::dotenv().ok();
+
+    let args = Config::parse();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    let middleware_stack = ServiceBuilder::new().layer(TraceLayer::new_for_http());
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(handler))
+        .layer(middleware_stack.into_inner());
+
+    // run it
+    let addr = SocketAddr::from((args.host, args.port));
+    tracing::debug!("listening on {}", addr);
+    let server = axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal());
+
+    if let Err(err) = server.await {
+        tracing::error!("server error: {:?}", err);
+    }
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::info!("signal received, starting graceful shutdown");
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
+}
