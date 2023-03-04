87a9556858c4655f9ad86d9752f32809d08211ce
vdbewout <vdbewout@gmail.com>
2023-03-04 15:11:59 +0100

    fix: replace FromRequest with FromRequestParts
    `FromRequest` requires ClaimDecoded<T> to the last param of an handler.
    This gives problems for types taht also only implement `FromRequest`.
    `FromRequestParts` does not have this problem.

--- api/src/extractor.rs
+++ api/src/extractor.rs
@@ -1,4 +1,6 @@
-use axum::{async_trait, extract::FromRequest, http::Request, TypedHeader};
+use axum::{
+    async_trait, extract::FromRequestParts, http::request::Parts, TypedHeader,
+};
 use headers::{authorization::Bearer, Authorization};
 use serde::Deserialize;
 use validator::Validate;
@@ -16,24 +18,27 @@ pub struct RequestUser {
     pub password: String,
 }
 
+// TODO: Lose the async_strait
+// it is possible with lifetimes, boxes and an async scope
 #[async_trait]
-impl<S, B, T> FromRequest<S, B> for ClaimsDecoded<T>
+impl<S, T> FromRequestParts<S> for ClaimsDecoded<T>
 where
-    B: Send + 'static,
     S: Send + Sync,
     T: ClaimsSubTrait,
     jwt::Decoded<T>: for<'a> Deserialize<'a>,
 {
     type Rejection = ApiError;
 
-    async fn from_request(
-        request: Request<B>,
+    async fn from_request_parts(
+        parts: &mut Parts,
         state: &S,
     ) -> Result<Self, Self::Rejection> {
         let TypedHeader(Authorization(bearer)) =
-            TypedHeader::<Authorization<Bearer>>::from_request(request, state)
-                .await
-                .map_err(ErrorRepr::MissingBearer)?;
+            TypedHeader::<Authorization<Bearer>>::from_request_parts(
+                parts, state,
+            )
+            .await
+            .map_err(ErrorRepr::MissingBearer)?;
 
         let token = bearer.token();
         let claims: ClaimsEncoded<T> = From::from(token.to_owned());
